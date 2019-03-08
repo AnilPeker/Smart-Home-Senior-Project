639275feca56eb6731f014bd123afbbb79dffee4
Temirlan94 <47722537+Temirlan94@users.noreply.github.com>
2019-03-08 19:05:36 +0200

    Add files via upload
    Writting to Database Added

--- R Language/tempTestR.r
+++ R Language/tempTestR.r
@@ -19,7 +19,7 @@ print(humidity)
 print(tempSum)
 
 temp.data<-data.frame(t=c(tempSum))
-
+dbClearResult(result)
 dbDisconnect(con)
 
 
@@ -28,4 +28,6 @@ con<-dbConnect(MySQL(),host="localhost",dbname="temp",user="root",password="")
 
 dbWriteTable(con, "temp_sum", temp.data,overwrite=TRUE)
 
+
+dbDisconnect(con)
 
