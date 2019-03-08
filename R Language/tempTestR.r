890f23f0eb0b020bedb9fc82d68a955270a57f6e
Temirlan94 <47722537+Temirlan94@users.noreply.github.com>
2019-03-08 19:12:36 +0200

    Add files via upload

--- R Language/tempTestR.r
+++ R Language/tempTestR.r
@@ -1,17 +1,21 @@
 library(DBI)
 library(RMySQL)
 
+#connection to database
+#-----------------------
 con<-dbConnect(MySQL(),host="localhost",dbname="temp",user="root",password="")
 result=dbSendQuery (con,"select * from temp")
 
+#-----------------------
+#Taking columns from database
+#-----------------------
 data=fetch(result,n=3)
-
 temp=subset(data,select=c(temp))
 
 humidity=subset(data,select=c(hum))
 
 tempSum<-(sum(temp))
-
+#------------------------
 print(data)
 print(temp)
 print(humidity)
@@ -19,10 +23,14 @@ print(humidity)
 print(tempSum)
 
 temp.data<-data.frame(t=c(tempSum))
+
+#closing and disconnetcting from database
+#--------------------
 dbClearResult(result)
 dbDisconnect(con)
 
-
+#writting back to database part
+#---------------------
 con<-dbConnect(MySQL(),host="localhost",dbname="temp",user="root",password="")
 
 
@@ -30,4 +38,6 @@ dbWriteTable(con, "temp_sum", temp.data,overwrite=TRUE)
 
 
 dbDisconnect(con)
+#----------------------
+
 
