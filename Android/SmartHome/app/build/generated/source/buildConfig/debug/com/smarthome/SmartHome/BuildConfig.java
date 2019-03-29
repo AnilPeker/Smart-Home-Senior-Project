8548a6e8243bbe5b49c792328b63ec8594778850
AnilPeker <anil.peker@outlook.com>
2019-03-29 03:29:09 +0200

    Update BuildConfig.java

--- Android/SmartHome/app/build/generated/source/buildConfig/debug/com/smarthome/SmartHome/BuildConfig.java
+++ Android/SmartHome/app/build/generated/source/buildConfig/debug/com/smarthome/SmartHome/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.smarthome.SmartHome;
+package com.smarthome.smarthome;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.smarthome.SmartHome";
+  public static final String APPLICATION_ID = "com.smarthome.smarthome";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
